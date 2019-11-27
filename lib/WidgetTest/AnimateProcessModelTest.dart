278064276974a9601d29da71b1aed2322adce490
zhangjiang <zhangjiang@qtrade.com.cn>
2019-11-27 11:13:13 +0800

    设置进度条的展示动画

--- lib/WidgetTest/AnimateProcessModelTest.dart
+++ lib/WidgetTest/AnimateProcessModelTest.dart
@@ -9,7 +9,29 @@ class AnimateProcessModelTest extends StatefulWidget {
 
 }
 
-class _AnimateProcessModelState extends State<AnimateProcessModelTest> {
+//这个class中添加了 SingleTickerProviderStateMixin这个协议
+class _AnimateProcessModelState extends State<AnimateProcessModelTest> with SingleTickerProviderStateMixin{
+
+  AnimationController _animationController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _animationController = new AnimationController(vsync: this,
+        duration: Duration(seconds: 3),
+    );
+    _animationController.forward();
+    _animationController.addListener(()=>setState(()=>{}));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -18,9 +40,9 @@ class _AnimateProcessModelState extends State<AnimateProcessModelTest> {
         title: Text("ProcessTest"),
       ),
       body: Padding(
-        padding: EdgeInsets.only(left: 0,top: 10,right: 0,bottom: 0),
+        padding: EdgeInsets.only(top: 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             //没有动画的进度条
             LinearProgressIndicator(
@@ -54,8 +76,10 @@ class _AnimateProcessModelState extends State<AnimateProcessModelTest> {
             ),
             //指定容器的宽高
             Column(
-              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
               children: <Widget>[
+                Padding(padding: EdgeInsets.only(top: 10),),
                 SizedBox(
                   height: 3,
                   child: LinearProgressIndicator(
@@ -64,6 +88,7 @@ class _AnimateProcessModelState extends State<AnimateProcessModelTest> {
                     valueColor: AlwaysStoppedAnimation(Colors.green),
                   ),
                 ),
+                Padding(padding: EdgeInsets.only(top: 10),),
                 SizedBox(
                   width: 50,
                   height: 50,
@@ -75,7 +100,33 @@ class _AnimateProcessModelState extends State<AnimateProcessModelTest> {
                 ),
               ],
             ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+            ),
+            Column(
 
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: 4,
+                  child: LinearProgressIndicator(
+                    backgroundColor: Colors.grey[200],
+                    valueColor: ColorTween(begin: Colors.grey,end: Colors.blue).animate(_animationController),
+                    value: _animationController.value,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 10),),
+                SizedBox(
+                  width: 100,
+                  height: 100,
+                  child: CircularProgressIndicator(
+                    value: _animationController.value,
+                    valueColor: ColorTween(begin: Colors.grey,end: Colors.red).animate(_animationController),
+                    backgroundColor: Colors.grey[200],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
